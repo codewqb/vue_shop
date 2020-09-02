09e16ed8d256c5a08fa15bc26120a121fab717ca
cedewqb <wangqb88888@163.com>
2020-09-02 19:32:06 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,18 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-  export default {
-    name: 'app',
-    components: {}
-  }
-
-=======
 export default {
-  name: 'app'
+  name: 'app',
 }
->>>>>>> edc44ad4353e52ea06aabffed4d60c7b09b605af
 </script>
 
 <style lang="less">
-  @import './assets/css/base.css';
+@import './assets/css/base.css';
 
-  html,
-  body,
-  #app {
-    height: 100%;
-    min-width: 1360px;
-  }
-
+html,
+body,
+#app {
+  height: 100%;
+  min-width: 1360px;
+}
 </style>
