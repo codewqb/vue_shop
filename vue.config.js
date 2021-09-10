e11dc640ae4707e80d9643b577a786dc4b593e57
codewqb <wangqb88888@163.com>
2021-09-10 14:32:11 +0800

    ...

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     proxy: {
       '/api': {
         // '/proxyApi'是代理标识，用于告诉node，url前面是/proxyApi的就是使用代理的
-        target: 'http://wqb.didiers.top', //目标地址，一般是指后台服务器地址
+        target: 'http://wqb.didiers.top/', //目标地址，一般是指后台服务器地址
         changeOrigin: true, //是否跨域
         pathRewrite: {
           // pathRewrite 的作用是把实际Request Url中的'/proxyApi'用""代替
