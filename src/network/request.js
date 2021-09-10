e0e6198fa1681759ff5be2f5291d8214acf8c13f
codewqb <wangqb88888@163.com>
2021-09-10 14:12:58 +0800

    修改配置

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ export function request(config) {
   // 创建axios实例
   const instance = axios.create({
     // 基准地址
-    baseURL: 'http://127.0.0.1:8888/api/private/v1/',
+    baseURL: '/api/private/v1/',
     // 超时
     timeout: 5000
   })
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,27 @@
 module.exports = {
+  devServer: {
+    open: true,
+    proxy: {
+      '/api': {
+        // '/proxyApi'是代理标识，用于告诉node，url前面是/proxyApi的就是使用代理的
+        target: 'http://wqb.didiers.top', //目标地址，一般是指后台服务器地址
+        changeOrigin: true, //是否跨域
+        pathRewrite: {
+          // pathRewrite 的作用是把实际Request Url中的'/proxyApi'用""代替
+          '^/api': 'api'
+        }
+      }
+    }
+  },
   configureWebpack: {
     resolve: {
       // 解决路径
       alias: {
-        'assets': '@/assets',
-        'common': '@/common',
-        'components': '@/components',
-        'network': '@/network',
-        'views': '@/views',
+        assets: '@/assets',
+        common: '@/common',
+        components: '@/components',
+        network: '@/network',
+        views: '@/views'
       }
     }
   }
